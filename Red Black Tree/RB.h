ba4dc4e1bbadf7d43b914576905aa29d91b8a935
georgen2003 <georgen2003@gmail.com>
2022-07-10 14:12:18 +0300

    Added VisitFunc

--- Red Black Tree/RB.h
+++ Red Black Tree/RB.h
@@ -1,6 +1,7 @@
 typedef void *Pointer; //Void pointer to store any kind of data needed
 typedef void(*DestroyFunc)(Pointer); //Function to propertly free the momory that our data are using
 typedef int(*CompareFunc)(Pointer a,Pointer b);//Returns 0 if a=b,<0 if a<b,>0 if a>b 
+typedef void(*VisitFunc)(Pointer);//Function to use when traversing the tree
 
 typedef struct redblack *RB; // Pointer to the entire tree
 
@@ -11,7 +12,7 @@ RB Initialize(DestroyFunc Destroy,CompareFunc Compare);
 int RemoveKey(RB tree, Pointer key); //Returns -1 in case the key is not found
 int InsertKey(RB tree, Pointer key); //Returns -1 in case the insertion failed
 NodePtr Search(RB tree, Pointer key);
-void PrintElements(RB tree,void(*Visit)(Pointer));
+void PrintElements(RB tree,VisitFunc);
 Pointer DataFromNode(NodePtr node);
 void print2D(RB tree);
 void RBFree(RB tree);
